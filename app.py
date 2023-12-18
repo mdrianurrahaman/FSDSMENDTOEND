d96069e939be18437b23e8faf99dcfd53960c5e7
rian <mdrianurrahaman@gmail.com>
2023-12-18 22:08:11 +0530

    app.py is updated

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
-import os 
-import sys 
-import pandas as pd 
-from src.DimondPricePrediction.exception import customexception
-from src.DimondPricePrediction.logger import logging
-from src.DimondPricePrediction.utils.utils import load_object
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
-class PredictPipeline:
-    def __init__(self):
-        pass
-    def predict(self):
-        try:
-            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
-            model_path=os.path.join("artifacts","model.pkl")
-        
+from flask import Flask,request,render_template,jsonify
 
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/predict",methods=["GET","POST"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("form.html")
+    
+    else:
+        data=CustomData(
             
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        # this is my final data
+        final_data=data.get_data_as_dataframe()
+        
+        predict_pipeline=PredictPipeline()
+        
+        pred=predict_pipeline.predict(final_data)
+        
+        result=round(pred[0],2)
+        
+        return render_template("result.html",final_result=result)
 
-
-        except Exception as e:
-             raise customexception(e,sys)
+#execution begin
+if __name__ == '__main__':
+    app.run()
